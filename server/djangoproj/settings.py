1e6527ce57efe352e97261b4f325c732e751deef
Aditya-2204 <william1binki@gmail.com>
2025-06-16 19:00:58 -0500

    Lab 6

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['https://williamadity-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/','localhost']
-CSRF_TRUSTED_ORIGINS=['https://williamadity-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS=['127.0.0.1']
+CSRF_TRUSTED_ORIGINS=[]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
